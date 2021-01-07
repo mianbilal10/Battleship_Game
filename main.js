1b6652a20d21407e710ed7070cbdc28e8f348b3a
Muhammad Bilal <mianmbilalpak@gmail.com>
2021-01-07 16:44:42 +0500

    Front-end completed and committed

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello and welcome to the main.js file!");
+var shot = function(coord){
+    var box = document.getElementById(coord);
+    if(hit(coord, "1")){
+        box.style.backgroundColor = "#d62828";
+        alert(coord + ": Congrates It's a Hit!");
+    }else{
+        box.style.backgroundColor = "white";
+        alert(coord + ": It's a Miss!");
+    }
+}
+
+var hit = function(coord, player){
+    return 1;
+}
